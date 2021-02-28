999c8a35e48fef65658344b87884d983db622e3b
wmsal80 <wmsal80@gmail.com>
2021-02-28 18:00:05 -0500

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# weather_dashboard
+This is a simple weather dashboard for planning future travels or just because not knowing the weather drives you insane.
---
+++ app.js
@@ -0,0 +1,4 @@
+//get function for searching for cities
+//get function to show history
+//get function to clear history
+//get function to show 5 day forecast
